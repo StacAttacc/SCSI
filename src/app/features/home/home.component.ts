2982ff6f5f65facdada3bb401cdb07892a22d64c
Anastasia <cookiedemons@outlook.com>
2026-08-13 19:29:13 -0400

    feat: why section

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './sections/hero/hero.component';
+import { WhyComponent } from './sections/why/why.component';
 import { ServicesComponent } from './sections/services/services.component';
 import { ContactComponent } from './sections/contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, ServicesComponent, ContactComponent],
+  imports: [HeroComponent, WhyComponent, ServicesComponent, ContactComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
