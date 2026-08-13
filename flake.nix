19b44a5afa425e0fe70620b40de7ef2c35cf095c
Anastasia <cookiedemons@outlook.com>
2026-08-12 22:18:07 -0400

    feat: adding nix flake and direnv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "SCSI Landing Page dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            nodePackages.npm
+            nodePackages."@angular/cli"
+          ];
+        };
+      });
+}
