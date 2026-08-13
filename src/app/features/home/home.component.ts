a5dd56886065360357ef8ff4188ecf5de5b44fb4
Anastasia <cookiedemons@outlook.com>
2026-08-13 18:24:00 -0400

    feat: hero component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './sections/hero/hero.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @Component({
   selector: 'app-home',
+  imports: [HeroComponent, ServicesComponent, ContactComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
